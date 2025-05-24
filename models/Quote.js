92cf564e1401d8635661c994f332d1fcfe88c8ae
sabrina <sabrinayen1301@gmail.com>
2025-05-24 22:41:48 +0800

    enhance api render

--- models/Quote.js
+++ models/Quote.js
@@ -6,4 +6,12 @@ const QuoteSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now },
 });
 
+QuoteSchema.set("toJSON", {
+  transform: (doc, ret) => {
+    ret._id = ret._id.toString();
+    delete ret.__v;
+    return ret;
+  },
+});
+
 export default mongoose.models.Quote || mongoose.model("Quote", QuoteSchema);
